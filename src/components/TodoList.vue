7292f9c14252883ee1fa5ce84ae44ea0a71880ae
milandadhaniya1 <milan.dadhaniya@gmail.com>
2023-07-18 16:57:45 +0530

    Fix the Migration Build's Errors

--- src/components/TodoList.vue
+++ src/components/TodoList.vue
@@ -5,7 +5,7 @@
       <div class="card-inner">
         <ul class="list-group">
           <li class="list-group-item d-flex p-3" v-for="(todo, index) in todos" :key="index">
-            <div class="flex-fill">{{ todo | capitalize }}</div>
+            <div class="flex-fill">{{ capitalizeTodoTitle(todo) }}</div>
             <div class="list-item-delete">
               <button type="button" @click="removeTodo(index)" class="btn btn-danger" title="Delete">X</button>
             </div>
@@ -20,14 +20,14 @@
 import { mapState, mapActions } from 'vuex';
 export default {
   name: "TodoList",
-  filters: {
-    capitalize(value) {
-      if (!value) return '';
-      return value.charAt(0).toUpperCase() + value.slice(1);
-    }
-  },
   computed: {
     ...mapState(['todos']),
+    capitalizeTodoTitle(){
+      return title => {
+        if (!title) return '';
+        return title.charAt(0).toUpperCase() + title.slice(1);
+      }
+    }
   },
   methods: {
     ...mapActions(['removeTodo']),
